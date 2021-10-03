e6ae066b8357b7313bca2a6c857d174f9717a58e
Mohammad Akhlaqur Rahman (2017831011) <43473062+tashfi04@users.noreply.github.com>
2021-10-03 10:33:30 +0600

    removed comments

--- go_rest_test/config/db_connect.go
+++ go_rest_test/config/db_connect.go
@@ -35,18 +35,6 @@ func Init() {
 		fmt.Printf("Unable to decode into struct, %v", err)
 	}
 
-	//// Reading variables using the model
-	//fmt.Println("Reading variables using the model..")
-	//fmt.Println("Database is\t", configuration.Database.DBName)
-	//fmt.Println("Port is\t\t", configuration.Server.Port)
-	//
-	//// Reading variables without using the model
-	//fmt.Println("\nReading variables without using the model..")
-	//fmt.Println("Database is\t", viper.GetString("database.dbname"))
-	//fmt.Println("Port is\t\t", viper.GetInt("server.port"))
-	//fmt.Println("EXAMPLE_PATH is\t", viper.GetString("EXAMPLE_PATH"))
-	//fmt.Println("EXAMPLE_VAR is\t", viper.GetString("EXAMPLE_VAR"))
-
 
 	dbSource := fmt.Sprintf("%s:%s@tcp(localhost:%d)/%s?charset=utf8",  configuration.Database.DBUser, configuration.Database.DBPassword, configuration.Server.Port, configuration.Database.DBName)
 
@@ -59,13 +47,3 @@ func Init() {
 	//defer Db.Close()
 
 }
-
-//func init() {
-//
-//	dbSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",  dbPass, dbHost, dbPort, dbName)
-//
-//	var err error
-//	db, err = sql.Open("mysql", dbSource)
-//
-//	catch(err)
-//}
--- go_rest_test/main.go
+++ go_rest_test/main.go
@@ -10,50 +10,7 @@ import (
 var Db *sql.DB
 
 func main() {
-	//// Set the file name of the configurations file
-	//viper.SetConfigName("config")
-	//
-	//// Set the path to look for the configurations file
-	//viper.AddConfigPath("..")
-	//
-	//// Enable VIPER to read Environment Variables
-	//viper.AutomaticEnv()
-	//
-	//viper.SetConfigType("yml")
-	//var configuration c.Configurations
-	//
-	//if err := viper.ReadInConfig(); err != nil {
-	//	fmt.Printf("Error reading config file, %s", err)
-	//}
-	//
-	//// Set undefined variables
-	//viper.SetDefault("database.dbname", "test_db")
-	//
-	//err := viper.Unmarshal(&configuration)
-	//if err != nil {
-	//	fmt.Printf("Unable to decode into struct, %v", err)
-	//}
-	//
-	//// Reading variables using the model
-	//fmt.Println("Reading variables using the model..")
-	//fmt.Println("Database is\t", configuration.Database.DBName)
-	//fmt.Println("Port is\t\t", configuration.Server.Port)
-	//
-	//// Reading variables without using the model
-	//fmt.Println("\nReading variables without using the model..")
-	//fmt.Println("Database is\t", viper.GetString("database.dbname"))
-	//fmt.Println("Port is\t\t", viper.GetInt("server.port"))
-	//fmt.Println("EXAMPLE_PATH is\t", viper.GetString("EXAMPLE_PATH"))
-	//fmt.Println("EXAMPLE_VAR is\t", viper.GetString("EXAMPLE_VAR"))
-	//
-	//
-	//dbSource := fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s?charset=utf8",  configuration.Database.DBUser, configuration.Database.DBPassword, configuration.Server.Port, configuration.Database.DBName)
-	//
-	//Db, err = sql.Open("mysql", dbSource)
-	//
-	//helpers.Catch(err)
-	//
-	//defer Db.Close()
+	
 
 	config.Init()
 
@@ -62,13 +19,3 @@ func main() {
 	http.ListenAndServe(":8000", r)
 
 }
-
-//func init() {
-//
-//	dbSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",  dbPass, dbHost, dbPort, dbName)
-//
-//	var err error
-//	db, err = sql.Open("mysql", dbSource)
-//
-//	catch(err)
-//}
